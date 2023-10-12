e3f0cb8d2d5d6f8251fe6397913005a42dad1554
koustav <koustavm.aeie2018@nsec.ac.in>
2023-10-12 16:36:45 +0530

    cmd5

--- src/app/api/auth/error/page.tsx
+++ src/app/api/auth/error/page.tsx
@@ -1,11 +1,18 @@
+import Link from 'next/link'
 import { FC } from 'react'
 
 interface pageProps {
-  
+
 }
 
-const page: FC<pageProps> = ({}) => {
-  return <div>page</div>
+const page: FC<pageProps> = ({ }) => {
+  return (
+    <div className="text-center">
+      <h2 className="text-3xl font-bold mb-4">Not Found</h2>
+      <p className="text-xl">Could not find the requested resource</p>
+      <a href="/" className="inline-block mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Return Home</a>
+    </div>
+  )
 }
 
 export default page
