4e3e42d73a613141f7f67a26ac2fd115297b613f
koustav <koustavm.aeie2018@nsec.ac.in>
2023-10-12 16:42:50 +0530

    cmd

--- src/app/api/auth/error/page.tsx
+++ src/app/api/auth/error/page.tsx
@@ -7,10 +7,12 @@ interface pageProps {
 
 const page: FC<pageProps> = ({ }) => {
   return (
-    <div className="text-center">
-      <h2 className="text-3xl font-bold mb-4">Not Found</h2>
-      <p className="text-xl">Could not find the requested resource</p>
-      <a href="/" className="inline-block mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Return Home</a>
+    <div className='flex min-h-screen justify-center items-center'>
+      <div className="text-center">
+        <h2 className="text-3xl font-bold mb-4">Not Found</h2>
+        <p className="text-xl">Could not find the requested resource</p>
+        <a href="/" className="inline-block mt-4 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">Return Home</a>
+      </div>
     </div>
   )
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -6,27 +6,8 @@ export default withAuth(
   async function middleware(req) {
     const pathname = req.nextUrl.pathname
 
-    // Manage route protection
-    const isAuth = await getToken({ req })
-
-    const isLoginPage = pathname.startsWith('/login')
-
-    const sensitiveRoutes = ['/dashboard']
-    const isAccessingSensitiveRoute = sensitiveRoutes.some((route) =>
-      pathname.startsWith(route)
-    )
-   
-    if (isLoginPage) {
-      if (isAuth) {
-        return NextResponse.redirect(new URL('/dashboard', req.url))
-      }
-
-      return NextResponse.next()
-    }
-
-    if (!isAuth && isAccessingSensitiveRoute) {
-      return NextResponse.redirect(new URL('/login', req.url))
-    }
+    // If the user is not authorized, redirect to login
+    
 
     if (pathname === '/') {
       return NextResponse.redirect(new URL('/dashboard', req.url))
