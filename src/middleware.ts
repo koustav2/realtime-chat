b6b7ad98d55c0d605fe392eaf2b1d028dc0dfa9c
koustav <koustavm.aeie2018@nsec.ac.in>
2023-10-12 18:05:30 +0530

    mm

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,45 +1,47 @@
-import { getToken } from 'next-auth/jwt';
-import { withAuth } from 'next-auth/middleware';
-import { NextResponse } from 'next/server';
+import { getToken } from 'next-auth/jwt'
+import { withAuth } from 'next-auth/middleware'
+import { NextResponse } from 'next/server'
 
 export default withAuth(
   async function middleware(req) {
-    const pathname = req.nextUrl.pathname;
+    const pathname = req.nextUrl.pathname
 
     // Manage route protection
-    const token = await getToken({ req });
-    const isLoginPage = pathname.startsWith('/login');
+    const isAuth = await getToken({ req })
+    // console.log('Token:', isAuth)
+    const isLoginPage = pathname.startsWith('/login')
 
-    const sensitiveRoutes = ['/dashboard'];
+    const sensitiveRoutes = ['/dashboard']
     const isAccessingSensitiveRoute = sensitiveRoutes.some((route) =>
       pathname.startsWith(route)
-    );
-
+    )
+   
     if (isLoginPage) {
-      if (token) {
-        return NextResponse.redirect('/dashboard');
+      if (isAuth) {
+        return NextResponse.redirect(new URL('/dashboard', req.url))
       }
 
-      return NextResponse.next();
+      return NextResponse.next()
     }
 
-    if (!token && isAccessingSensitiveRoute) {
-      return NextResponse.redirect('/login');
+    if (!isAuth && isAccessingSensitiveRoute) {
+      return NextResponse.redirect(new URL('/login', req.url))
     }
 
     if (pathname === '/') {
-      return NextResponse.redirect('/dashboard');
+      return NextResponse.redirect(new URL('/dashboard', req.url))
     }
   },
   {
     callbacks: {
       async authorized() {
-        return true;
+        return true
       },
     },
   }
-);
+)
 
 export const config = {
   matcher: ['/', '/login', '/dashboard/:path*'],
-};
+}
+
