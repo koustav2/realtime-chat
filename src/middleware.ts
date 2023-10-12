cc5b8d3da8a1aa2815927dba5c51ccf0e1553333
koustav <koustavm.aeie2018@nsec.ac.in>
2023-10-12 18:35:29 +0530

    ddvdv

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,11 +2,12 @@ import { getToken } from 'next-auth/jwt'
 import { withAuth } from 'next-auth/middleware'
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
-// export default withAuth(
-  async function middleware(req: NextRequest) {
-    return NextResponse.rewrite(new URL('/dashboard', req.url))
-  }
-// )
+
+export function middleware(request: NextRequest) {
+  return NextResponse.rewrite(new URL('/dashboard', request.url))
+}
+
+
 
 export const config = {
   matcher: ['/', '/login', '/dashboard/:path*'],
